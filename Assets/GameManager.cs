c549b4a09e3dc24158fd91b48e4c8fe6035a7dc2
homty <genndiiforjob@gmail.com>
2025-04-28 11:08:44 +0200

    initial commit

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -28,6 +28,22 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void Spin()
+    {
+        for (int i = 0; i < FieldHeight; i++)
+        {
+            for (int j = 0; j < FieldWidth; j++)
+            {
+                GameObject gridPosition = _field.transform.Find(i + " " + j).gameObject;
+                GameObject pieceType = gamePieces[Random.Range(0, gamePieces.Length)];
+                GameObject thisPiece = Instantiate(pieceType, gridPosition.transform.position + _offset, Quaternion.identity);
+                thisPiece.name = pieceType.name;
+                thisPiece.transform.parent = gridPosition.transform;
+                _SlotField[i, j] = thisPiece;
+            }
+        }
+    }
+
     void Update()
     {
         
