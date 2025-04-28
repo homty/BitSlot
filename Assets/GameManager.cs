c890a2574cb64c7ee806dd105df490a61a983ea0
homty <genndiiforjob@gmail.com>
2025-04-28 19:41:17 +0200

    Created spin logic and also added tokens to slot

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,47 +3,53 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private float FieldHeight, FieldWidth;
+    private int boardHeight, boardWidth;
     [SerializeField]
     private GameObject[] gamePieces;
-    private GameObject _field;
-    private GameObject[,] _SlotField;
-    private Vector3 _offset = new Vector3(0, 0, -1);
 
+    private GameObject _board;
+    private GameObject[,] _gameBoard;
+    private Vector3 _offset = new Vector3(0, 0, -1);
+    // Start is called before the first frame update
     void Start()
     {
-        _field = GameObject.Find("SlotField");
-        _SlotField = new GameObject[(int)FieldHeight, (int)FieldWidth];
-        for (int i = 0; i < FieldHeight; i++)
+        _board = GameObject.Find("GameBoard");
+        _gameBoard = new GameObject[boardHeight, boardWidth];
+        for (int i = 0; i < boardHeight; i++)
         {
-            for (int j = 0; j < FieldWidth; j++)
+            for (int j = 0; j < boardWidth; j++)
             {
-                GameObject gridPosition = _field.transform.Find(i + " " + j).gameObject;
+                GameObject gridPosition = _board.transform.Find(i + " " + j).gameObject;
                 GameObject pieceType = gamePieces[Random.Range(0, gamePieces.Length)];
                 GameObject thisPiece = Instantiate(pieceType, gridPosition.transform.position + _offset, Quaternion.identity);
                 thisPiece.name = pieceType.name;
                 thisPiece.transform.parent = gridPosition.transform;
-                _SlotField[i, j] = thisPiece;
+                _gameBoard[i, j] = thisPiece;
             }
         }
     }
-
     public void Spin()
     {
-        for (int i = 0; i < FieldHeight; i++)
+        for (int i = 0; i < boardHeight; i++)
         {
-            for (int j = 0; j < FieldWidth; j++)
+            for (int j = 0; j < boardWidth; j++)
             {
-                GameObject gridPosition = _field.transform.Find(i + " " + j).gameObject;
+                GameObject gridPosition = _board.transform.Find(i + " " + j).gameObject;
+                if (gridPosition.transform.childCount > 0)
+                {
+                    GameObject destroyPiece = gridPosition.transform.GetChild(0).gameObject;
+                    Destroy(destroyPiece);
+                }
                 GameObject pieceType = gamePieces[Random.Range(0, gamePieces.Length)];
                 GameObject thisPiece = Instantiate(pieceType, gridPosition.transform.position + _offset, Quaternion.identity);
                 thisPiece.name = pieceType.name;
                 thisPiece.transform.parent = gridPosition.transform;
-                _SlotField[i, j] = thisPiece;
+                _gameBoard[i, j] = thisPiece;
             }
         }
     }
 
+    // Update is called once per frame
     void Update()
     {
         
